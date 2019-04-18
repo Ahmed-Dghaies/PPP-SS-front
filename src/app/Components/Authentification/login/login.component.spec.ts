a1b2b5e3481e3172b65ed5dbbe083656314cc671
Ahmed-Dghaies <dghais.ahmed7@gmail.com>
2019-04-18 23:57:09 +0100

    fixing ng lint errors

--- src/app/Components/Authentification/login/login.component.spec.ts
+++ src/app/Components/Authentification/login/login.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './login.component';
 import { AuthService } from 'src/app/shared/services/auth.service';
+import { HttpClient } from '@angular/common/http';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -10,7 +11,8 @@ describe('LoginComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        FormsModule
+        FormsModule,
+        HttpClient
       ],
       declarations: [LoginComponent],
       providers: [AuthService]
