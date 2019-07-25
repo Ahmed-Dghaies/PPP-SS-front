0c30371232acb005eb3f3c4d0b63f1dabf12d8bd
Ahmed-Dghaies <dghais.ahmed7@gmail.com>
2019-07-25 23:24:58 +0100

    changing steg et autre folder name

---
+++ src/app/shared/models/espece.ts
@@ -0,0 +1,11 @@
+export class Espece {
+  constructor() {
+    this.type = 0;
+    this.quantity = 0;
+    this.state = '';
+  }
+  public _id?: string;
+  public type: number;
+  public quantity: number;
+  public state: string;
+}
